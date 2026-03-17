bd1521382fd14e5041333b78c0405e562628134e
joegamers <joel22sd@gmail.com>
2026-03-17 10:47:15 -0400

    Update `VITE_APP_VERSION` in `.env` and configure Vite to dynamically define it from `package.json`."

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(pkg.version),
+  },
   plugins: [
     react(),
     VitePWA({
